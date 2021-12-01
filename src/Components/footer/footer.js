5889b733f338fb871c0a0f1454cf674a80a21a7c
nanak <nanaksethh@gmail.com>
2021-12-01 23:37:25 +0530

    inital commit

---
+++ src/Components/footer/footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './footer.css'
+const Footer= ()=>{
+    return(
+        <footer className="footer dark-footer dark-bg">
+				
+        <div class="footer-copyright">
+            <p>Copyright@ 2021 Yardcan Maintained By riolabz</p>
+        </div>
+    </footer>
+    )
+}
+export default Footer
