ddb686baf9e172f5c9da2163397b28e878e9dab6
juned03 <alikhanjuned807@gmail.com>
2021-12-06 14:57:10 +0530

    [ADDED] some new pages and stuff

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Header.css";
-import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
-import { Navbar, Nav, Form, Button, Modal } from "react-bootstrap";
+import Modal from "../Modal/Modal";
+import { Navbar, Nav, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const Header = () => {
-  const [show, setShow] = useState(false);
   return (
     <>
       <Navbar bg="white" fixed="top" expand="lg" className="m-0 p-0">
@@ -35,82 +34,7 @@ const Header = () => {
             </Nav.Link>
           </Nav>
           <Form className="d-flex">
-            <Button
-              variant="btn"
-              style={{
-                height: "100%",
-                padding: "12px",
-                backgroundColor: "#1EFFAC",
-                color: "white",
-                boxShadow: "none",
-              }}
-              onClick={() => setShow(true)}
-            >
-              <PersonOutlineIcon />
-              Login/Signup
-            </Button>
-            <Modal
-              show={show}
-              onHide={() => setShow(false)}
-              dialogClassName="modal-90w"
-              aria-labelledby="example-custom-modal-styling-title"
-            >
-              <Modal.Header closeButton>
-                <Modal.Title id="example-custom-modal-styling-title">
-                  LogIn Your Account
-                </Modal.Title>
-              </Modal.Header>
-              <Modal.Body>
-                <div
-                  className="heading-container text-center"
-                  style={{ color: "#334E6F" }}
-                >
-                  <h1>
-                    Welcome <span style={{ color: "#1EFFAC" }}>Back!</span>
-                  </h1>
-                </div>
-                <div className="py-3 px-3">
-                  <div className="my-2">
-                    <Form.Label>Username</Form.Label>
-                    <Form.Control
-                      size="lg"
-                      type="text"
-                      placeholder="username...."
-                    />
-                  </div>
-                  <div className="my-2">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control
-                      size="lg"
-                      type="password"
-                      placeholder="********"
-                    />
-                  </div>
-                  <div className="my-2">
-                    <Form.Check
-                      type="checkbox"
-                      id="checkbox"
-                      label="Keep me SIgned in"
-                    />
-                  </div>
-                  <div className="mt-5 d-flex justify-content-center">
-                    <Button
-                      variant="btn"
-                      style={{
-                        height: "100%",
-                        padding: "1rem 4rem",
-                        backgroundColor: "#1EFFAC",
-                        color: "white",
-                        boxShadow: "none",
-                        borderRadius: "2rem",
-                      }}
-                    >
-                      Login
-                    </Button>
-                  </div>
-                </div>
-              </Modal.Body>
-            </Modal>
+            <Modal />
           </Form>
         </Navbar.Collapse>
       </Navbar>
--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -1,28 +1,113 @@
-import React from "react";
-import { Modal } from "react-bootstrap";
-
+import React, { useState } from "react";
+import { Button, Modal, Form } from "react-bootstrap";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
 const LBModal = () => {
+  const [show, setShow] = useState(false);
   return (
     <>
+      <Button
+        variant="btn"
+        style={{
+          height: "100%",
+          padding: "12px",
+          backgroundColor: "#1EFFAC",
+          color: "white",
+          boxShadow: "none",
+        }}
+        onClick={() => setShow(true)}
+      >
+        <PersonOutlineIcon />
+        Login/Signup
+      </Button>
+
       <Modal
-        dialogClassName="modal-90w"
+        size="md"
+        show={show}
+        onHide={() => setShow(false)}
+        dialogClassName="modal-150w"
         aria-labelledby="example-custom-modal-styling-title"
       >
         <Modal.Header closeButton>
-          <Modal.Title id="example-custom-modal-styling-title">
-            Custom Modal Styling
+          <Modal.Title
+            id="example-custom-modal-styling-title"
+            style={{ fontWeight: 400 }}
+          >
+            LogIn Your Account
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <p>
-            Ipsum molestiae natus adipisci modi eligendi? Debitis amet quae unde
-            commodi aspernatur enim, consectetur. Cumque deleniti temporibus
-            ipsam atque a dolores quisquam quisquam adipisci possimus
-            laboriosam. Quibusdam facilis doloribus debitis! Sit quasi quod
-            accusamus eos quod. Ab quos consequuntur eaque quo rem! Mollitia
-            reiciendis porro quo magni incidunt dolore amet atque facilis ipsum
-            deleniti rem!
-          </p>
+          <div
+            className="heading-container text-center"
+            style={{ color: "#334E6F" }}
+          >
+            <h1>
+              Welcome <span style={{ color: "#1EFFAC" }}>Back!</span>
+            </h1>
+          </div>
+          <div className="py-4 px-3">
+            <div className="my-2">
+              <Form.Label style={{ fontWeight: 500, color: "#798092" }}>
+                User Name
+              </Form.Label>
+              <Form.Control
+                size="lg"
+                type="text"
+                placeholder="username"
+                style={{ fontSize: "16px" }}
+              />
+            </div>
+            <div className="my-2">
+              <Form.Label style={{ fontWeight: 500, color: "#798092" }}>
+                Password
+              </Form.Label>
+              <Form.Control
+                size="lg"
+                type="password"
+                placeholder="********"
+                style={{ fontSize: "16px" }}
+              />
+            </div>
+            <div className="my-2">
+              <Form.Check
+                isValid
+                type="checkbox"
+                id="checkbox"
+                label="Keep me SIgned in"
+                style={{
+                  boxShadow: "none",
+                  color: "#798092",
+                }}
+              />
+            </div>
+            <div className="mt-5 d-flex justify-content-center">
+              <Button
+                variant="btn"
+                style={{
+                  height: "100%",
+                  padding: "0.8rem 4.8rem",
+                  backgroundColor: "#1EFFAC",
+                  color: "white",
+                  boxShadow: "none",
+                  borderRadius: "1.9rem",
+                }}
+              >
+                Login
+              </Button>
+            </div>
+            <div className="text-center">
+              <p>Don't have an account</p>
+              <Button
+                className="p-0"
+                variant="btn"
+                style={{
+                  color: "#1EFFAC",
+                  boxShadow: "none",
+                }}
+              >
+                Create an Account
+              </Button>
+            </div>
+          </div>
         </Modal.Body>
       </Modal>
     </>
